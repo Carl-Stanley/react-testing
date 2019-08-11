7bf215e0cc248c5497d9ea4508fd5d14c79329c1
Carl-Stanley <cds.beml@gmail.com>
2019-08-11 15:50:22 -0700

    Commit

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Table from './Table'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello, React!</h1>
+        <div><Table/></div>
       </div>
+      
     )
   }
 }
